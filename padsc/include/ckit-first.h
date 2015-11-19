587a3b64b9afe232d5be299a3953b15e37616457
Kathleen Fisher <kathleen.fisher@gmail.com>
2015-11-19 16:58:47 -0500

    fixed typo

--- padsc/include/ckit-first.h
+++ padsc/include/ckit-first.h
@@ -3,7 +3,7 @@
 
 #ifdef __LINUX_PREPROCESSOR_FIXES
 typedef void * __builtin_va_list;
-typedef void * vva_list;
+typedef void * va__list;
 #define __THROW
 /* extern int ftruncate (int __fd, long int __length) ; */
 
