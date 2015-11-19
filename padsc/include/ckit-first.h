bb3fae3d7b5ee7ade0e389e15826b14195a73d30
Kathleen Fisher <kathleen.fisher@gmail.com>
2015-11-19 17:54:34 -0500

    fixed include guard

--- padsc/include/ckit-first.h
+++ padsc/include/ckit-first.h
@@ -1,9 +1,13 @@
 /* ================================================================================ */
 /* XXX THESE ARE REQUIRED FOR CKIT ON REDHAT LINUX 7.3 (AND MAYBE OTHERS) */
 
+#ifdef HAVE_VA_LIST
+typedef void * va_list;
+#endif
+
+
 #ifdef __LINUX_PREPROCESSOR_FIXES
 typedef void * __builtin_va_list;
-typedef void * va_list;
 #define __THROW
 /* extern int ftruncate (int __fd, long int __length) ; */
 
