b288fd25caf0852b0a7a76941b4cef3d41637f3b
Kathleen Fisher <kathleen.fisher@gmail.com>
2015-11-19 17:10:00 -0500

    really fixed it this time

--- padsc/include/ckit-first.h
+++ padsc/include/ckit-first.h
@@ -3,7 +3,7 @@
 
 #ifdef __LINUX_PREPROCESSOR_FIXES
 typedef void * __builtin_va_list;
-typedef void * va__list;
+typedef void * va_list;
 #define __THROW
 /* extern int ftruncate (int __fd, long int __length) ; */
 
